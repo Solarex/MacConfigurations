80792d8e7f55a73df17f64ffc333f9f526d159f2
侯茹厚 <houruhou@meizu.com>
2016-01-06 17:00:39 +0800

    Squashed 'bitbar/' content from commit 9936243
    git-subtree-dir: bitbar
    git-subtree-split: 9936243ff43b7442ed3b9610b34d87770205e91b

---
+++ App/BitBar/Plugin.m
@@ -0,0 +1,418 @@
+//
+//  Plugin.m
+//  BitBar
+//
+//  Created by Mat Ryer on 11/12/13.
+//  Copyright (c) 2013 Bit Bar. All rights reserved.
+//
+
+#import "Plugin.h"
+#import "PluginManager.h"
+#import "STPrivilegedTask.h"
+#import "NSDate+DateTools.h"
+#import "NSColor+Hex.h"
+
+#define DEFAULT_TIME_INTERVAL_SECONDS ((double)60.)
+
+@implementation Plugin
+
+- init { return self = super.init ? _currentLine = -1, _cycleLinesIntervalSeconds = 5, self : nil; }
+
+- initWithManager:(PluginManager*)manager { return self = self.init ? _manager = manager, self : nil; }
+
+- (NSStatusItem *)statusItem { return _statusItem = _statusItem ?: ({
+    
+    // make the status item
+    _statusItem = [self.manager.statusBar statusItemWithLength:NSVariableStatusItemLength];
+    
+    // build the menu
+    [self rebuildMenuForStatusItem:_statusItem]; _statusItem; });
+  
+}
+
+- (NSMenuItem*) buildMenuItemWithParams:(NSDictionary *)params {
+
+  NSString * title = [params objectForKey:@"title"];
+  SEL sel = params[@"href"] ? @selector(performMenuItemHREFAction:)
+          : params[@"bash"] ? @selector(performMenuItemOpenTerminalAction:) : nil;
+
+  NSMenuItem * item = [NSMenuItem.alloc initWithTitle:title action:sel keyEquivalent:@""];
+  if (sel) {
+    item.representedObject = params;
+    [item setTarget:self];
+  }
+  if (params[@"size"] || params[@"color"])
+    item.attributedTitle = [self attributedTitleWithParams:params];
+
+  return item;
+}
+
+- (NSAttributedString*) attributedTitleWithParams:(NSDictionary *)params {
+
+  NSString * title = params[@"title"];
+  CGFloat     size = params[@"size"] ? [params[@"size"] floatValue] : 14;
+  NSFont    * font = params[@"font"] ? [NSFont fontWithName:params[@"font"] size:size]
+                                     : [NSFont menuFontOfSize:size]
+                                    ?: [NSFont menuFontOfSize:size];
+  NSColor * fgColor;
+  NSMutableAttributedString * attributedTitle = [NSMutableAttributedString.alloc initWithString:title attributes:@{NSFontAttributeName: font}];
+  if (!params[@"color"]) return attributedTitle;
+  if ((fgColor = [NSColor colorWithWebColorString:[params objectForKey:@"color"]]))
+    [attributedTitle addAttribute:NSForegroundColorAttributeName value:fgColor range:NSMakeRange(0, title.length)];
+  return attributedTitle;
+}
+
+- (NSMenuItem*) buildMenuItemForLine:(NSString *)line { return [self buildMenuItemWithParams:[self dictionaryForLine:line]]; }
+
+- (NSDictionary*) dictionaryForLine:(NSString *)line {
+  // Find the title
+  NSRange found = [line rangeOfString:@"|"];
+  if (found.location == NSNotFound) return @{ @"title": line };
+  NSString * title = [[line substringToIndex:found.location] stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceCharacterSet];
+  NSMutableDictionary * params = @{@"title":title}.mutableCopy;
+  
+  // Find the parameters
+  NSString * paramStr = [line substringFromIndex:found.location + found.length];
+
+  NSScanner* scanner = [NSScanner scannerWithString:paramStr];
+  NSMutableCharacterSet* keyValueSeparator = [NSMutableCharacterSet characterSetWithCharactersInString:@"=:"];
+  NSMutableCharacterSet* quoteSeparator = [NSMutableCharacterSet characterSetWithCharactersInString:@"\"'"];
+  
+  while (![scanner isAtEnd]) {
+    NSString *key = @""; NSString* value = @"";
+    [scanner scanUpToCharactersFromSet:keyValueSeparator intoString:&key];
+    [scanner scanCharactersFromSet:keyValueSeparator intoString:NULL];
+    
+    if ([scanner scanCharactersFromSet:quoteSeparator intoString:NULL]) {
+      [scanner scanUpToCharactersFromSet:quoteSeparator intoString:&value];
+      [scanner scanCharactersFromSet:quoteSeparator intoString:NULL];
+    } else {
+      [scanner scanUpToString:@" " intoString:&value];
+    }
+    
+    // Remove extraneous spaces from key and value
+    key = [key stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    value = [value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    params[key] = value;
+    
+    if([key isEqualToString:@"args"]){
+      params[key] = [value componentsSeparatedByString:@"__"];
+    }
+  }
+  
+  return params;
+}
+
+- (void) performMenuItemHREFAction:(NSMenuItem *)menuItem {
+
+  [NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:menuItem.representedObject[@"href"]]];
+}
+
+
+- (void) performMenuItemOpenTerminalAction:(NSMenuItem *)menuItem {
+
+    NSMutableDictionary * params = menuItem.representedObject;
+    NSString *bash = [params[@"bash"] stringByStandardizingPath].stringByResolvingSymlinksInPath,
+           *param1 = params[@"param1"] ?: @"",
+           *param2 = params[@"param2"] ?: @"",
+           *param3 = params[@"param3"] ?: @"",
+           *param4 = params[@"param4"] ?: @"",
+           *param5 = params[@"param5"] ?: @"",
+         *terminal = params[@"terminal"] ?: [NSString stringWithFormat:@"%s", "true"];
+    NSArray *args = params[@"args"] ?: ({
+
+      NSMutableArray *argArray = @[].mutableCopy;
+      for (int i = 1; i < 6; i ++) {
+        id x = params[[NSString stringWithFormat:@"param%i", i]];
+        if (x) [argArray addObject:x];
+      }
+      argArray.copy;
+
+    });
+    
+    if([terminal isEqual: @"false"]){
+
+      NSLog(@"Args: %@", args);
+
+      id task = [params[@"root"] isEqualToString:@"true"] ? STPrivilegedTask.new : NSTask.new;
+
+      [(NSTask*)task setLaunchPath:bash];
+      [(NSTask*)task setArguments:args];
+      [(NSTask*)task launch];
+
+    } else {
+
+      NSString *full_link = [NSString stringWithFormat:@"%@ %@ %@ %@ %@ %@", bash, param1, param2, param3, param4, param5];
+      NSString *s = [NSString stringWithFormat:@"tell application \"Terminal\" \n\
+                 activate \n\
+                 if length of (get every window) is 0 then \n\
+                 tell application \"System Events\" to tell process \"Terminal\" to click menu item \"New Window\" of menu \"File\" of menu bar 1 \n\
+                 end if \n\
+                 do script \"%@\" in front window activate \n\
+                 end tell", full_link];
+      NSAppleScript *as = [NSAppleScript.alloc initWithSource: s];
+      [as executeAndReturnError:nil];
+    }
+}
+
+- (void) rebuildMenuForStatusItem:(NSStatusItem*)statusItem {
+  
+  // build the menu
+  NSMenu *menu = NSMenu.new;
+  [menu setDelegate:self];
+  
+  if (self.isMultiline) {
+    
+    // put all content as an item
+    NSString *line;
+    for (line in self.allContentLines) {
+      NSMenuItem * item = [self buildMenuItemForLine:line];
+      [menu addItem:item];
+    }
+    
+    // add the seperator
+    [menu addItem:[NSMenuItem separatorItem]];
+    
+    // are there any allContentLinesAfterBreak?
+    if (self.allContentLinesAfterBreak.count > 0) {
+      
+      // put all content as an item
+      NSString *line;
+      for (line in self.allContentLinesAfterBreak) {
+        
+        if ([line isEqualToString:@"---"]) {
+          [menu addItem:[NSMenuItem separatorItem]];
+        } else {
+          NSMenuItem * item = [self buildMenuItemForLine:line];
+          [menu addItem:item];
+        }
+        
+      }
+      
+      // add the seperator
+      [menu addItem:[NSMenuItem separatorItem]];
+      
+    }
+    
+  }
+  
+  if (self.lastUpdated != nil) {
+    
+    self.lastUpdatedMenuItem = [NSMenuItem.alloc initWithTitle:@"Updated just now" action:nil keyEquivalent:@""];
+    [menu addItem:self.lastUpdatedMenuItem];
+    
+    [menu addItem:[NSMenuItem separatorItem]];
+  }
+  
+  [self addAdditionalMenuItems:menu];
+  [self addDefaultMenuItems:menu];
+  
+  // set the menu
+  statusItem.menu = menu;
+  
+}
+
+- (void) addDefaultMenuItems:(NSMenu *)menu {
+  if (menu.itemArray.count>0) {
+    [menu addItem:[NSMenuItem separatorItem]];
+  }
+  [self.manager addHelperItemsToMenu:menu asSubMenu:(menu.itemArray.count>0)];
+  
+}
+
+- (void) addAdditionalMenuItems:(NSMenu *)menu { }
+
+- (void) changePluginsDirectorySelected:_ {
+  
+  _manager.path = nil;
+  [_manager reset];
+}
+
+- (NSNumber*) refreshIntervalSeconds {
+  
+  if (_refreshIntervalSeconds == nil) {
+    
+    NSArray *segments = [self.name componentsSeparatedByString:@"."];
+    
+    if (segments.count < 3)
+      return _refreshIntervalSeconds = @(DEFAULT_TIME_INTERVAL_SECONDS);
+    
+    NSString *timeStr = [segments[1] lowercaseString];
+    
+    if ([timeStr length] < 2) {
+      _refreshIntervalSeconds = @(DEFAULT_TIME_INTERVAL_SECONDS);
+      return _refreshIntervalSeconds;
+    }
+    
+    NSString *numberPart = [timeStr substringToIndex:[timeStr length]-1];
+    double numericalValue = numberPart.doubleValue ?: DEFAULT_TIME_INTERVAL_SECONDS;
+    
+    if ([timeStr hasSuffix:@"s"]) {
+      // this is ok - but nothing to do
+    } else if ([timeStr hasSuffix:@"m"]) numericalValue *= 60;
+      else if ([timeStr hasSuffix:@"h"]) numericalValue *= 60*60;
+      else if ([timeStr hasSuffix:@"d"]) numericalValue *= 60*60*24;
+      else
+      return _refreshIntervalSeconds = @(DEFAULT_TIME_INTERVAL_SECONDS);
+
+    
+    _refreshIntervalSeconds = @(numericalValue);
+    
+  }
+  
+  return _refreshIntervalSeconds;
+  
+}
+
+- (BOOL) refresh {
+  return YES;
+}
+
+- (NSString*) lastUpdatedString { return [self.lastUpdated timeAgoSinceNow].lowercaseString; }
+
+- (void) cycleLines {
+  
+  // do nothing if the menu is open
+  if (self.menuIsOpen) { return; };
+  
+  // update the status item
+  self.currentLine++;
+  
+  // if we've gone too far - wrap around
+  if ((NSUInteger)self.currentLine >= self.allContentLines.count) {
+    self.currentLine = 0;
+  }
+  
+  if (self.allContentLines.count > 0) {
+    NSDictionary * params = [self dictionaryForLine:self.allContentLines[self.currentLine]];
+    self.statusItem.attributedTitle = [self attributedTitleWithParams:params];
+    self.pluginIsVisible = YES;
+  } else {
+    self.statusItem = nil;
+    self.pluginIsVisible = NO;
+  }
+  
+}
+
+- (void) contentHasChanged {
+  _allContent = nil;
+  _allContentLines = nil;
+  _allContentLinesAfterBreak = nil;
+}
+
+- (void) setContent:(NSString *)content {
+  _content = content;
+  [self contentHasChanged];
+}
+
+- (void) setErrorContent:(NSString *)errorContent {
+  _errorContent = errorContent;
+  [self contentHasChanged];
+}
+
+- (NSString *)allContent {
+  if (!_allContent) {
+    _allContent = self.content;
+    if (self.errorContent.length > 0) {
+      _allContent = [@"⚠️" stringByAppendingString:_allContent];
+      _allContent = [_allContent stringByAppendingString:@"\n---\n"];
+      _allContent = [_allContent stringByAppendingString:self.errorContent];
+    }
+  }
+  return _allContent;
+}
+
+- (NSArray *)allContentLines {
+  
+  return _allContentLines = _allContentLines ?: ({
+
+    NSMutableArray *cleanLines = @[].mutableCopy;
+    
+    for (NSString *lineEval in [self.allContent componentsSeparatedByCharactersInSet:NSCharacterSet.newlineCharacterSet]) {
+
+
+
+      
+      // strip whitespace
+      NSString *line = [self cleanLine:lineEval];
+
+      // add the line if we have something in it
+      if (line.length) {
+      
+        if ([line isEqualToString:@"---"]) break;
+        
+        [cleanLines addObject:line];
+      
+      }
+      
+    }
+    cleanLines.copy;
+  });
+  
+}
+
+- (NSString*) cleanLine:(NSString*)line {
+    return line;
+  NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"  +" options:NSRegularExpressionCaseInsensitive error:nil];
+  return [regex stringByReplacingMatchesInString:line options:0 range:NSMakeRange(0, line.length) withTemplate:@" "];
+}
+
+- (NSArray*) allContentLinesAfterBreak {
+  
+  if (_allContentLinesAfterBreak == nil) {
+    
+    NSArray *lines = [self.allContent componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSMutableArray *cleanLines = [NSMutableArray.alloc initWithCapacity:lines.count];
+    NSString *line;
+    BOOL storing = NO;
+
+    for (line in lines) {
+      
+      // strip whitespace
+      line = [self cleanLine:line];
+      
+      // add the line if we have something in it
+      if (line.length > 0) {
+        
+        if ([line isEqualToString:@"---"]) {
+          
+          if (storing) {
+            [cleanLines addObject:line];
+          }
+          
+          storing = YES;
+        } else {
+          if (storing == YES) {
+            [cleanLines addObject:line];
+          }
+        }
+        
+      }
+      
+    }
+    
+    _allContentLinesAfterBreak = [NSArray arrayWithArray:cleanLines];
+    
+  }
+  
+  return _allContentLinesAfterBreak;
+  
+}
+
+- (BOOL) isMultiline {
+  return [self.allContentLines count] > 1 || [self.allContentLinesAfterBreak count]>0;
+}
+
+#pragma mark - NSMenuDelegate
+
+- (void)menuWillOpen:(NSMenu *)menu {
+  self.menuIsOpen = YES;
+  [self.statusItem setHighlightMode:YES];
+}
+
+- (void)menuDidClose:(NSMenu *)menu {
+  self.menuIsOpen = NO;
+  [self.statusItem setHighlightMode:NO];
+}
+
+@end
---
+++ App/BitBar/PluginManager.m
@@ -0,0 +1,309 @@
+//
+//  PluginManager.m
+//  BitBar
+//
+//  Created by Mat Ryer on 11/12/13.
+//  Copyright (c) 2013 Bit Bar. All rights reserved.
+//
+
+#import "PluginManager.h"
+#import "Plugin.h"
+#import "ExecutablePlugin.h"
+#import "HTMLPlugin.h"
+#import "NSUserDefaults+Settings.h"
+#import "LaunchAtLoginController.h"
+
+@implementation PluginManager
+
+- initWithPluginPath:(NSString*)path {
+
+  return self = super.init ? _path = path.stringByStandardizingPath, self : nil;
+}
+
+- (void) showSystemStatusItemWithMessage:(NSString*)message {
+  
+  [self.statusBar removeStatusItem:self.defaultStatusItem];
+  
+  // make default menu item
+  self.defaultStatusItem = [self.statusBar statusItemWithLength:NSVariableStatusItemLength];
+  [self.defaultStatusItem setTitle:NSProcessInfo.processInfo.processName];
+  [(self.defaultStatusItem.menu = NSMenu.new) setDelegate:self];
+  
+  if (message.length) {
+    NSMenuItem *messageMenuItem = [NSMenuItem.alloc initWithTitle:message action:nil keyEquivalent:@""];
+    [self.defaultStatusItem.menu addItem:messageMenuItem];
+    [self.defaultStatusItem.menu addItem:NSMenuItem.separatorItem];
+  }
+
+  [self addHelperItemsToMenu:self.defaultStatusItem.menu asSubMenu:NO];
+  
+}
+
+#define ADD_MENU(TITLE,SELECTOR,SHORTCUT,TARGET) ({ \
+  NSMenuItem *item = [NSMenuItem.alloc initWithTitle:TITLE action:NSStringFromSelector(@selector(SELECTOR)) ? @selector(SELECTOR) : NULL keyEquivalent:SHORTCUT?:@""];\
+  if (TARGET) [item setTarget:TARGET]; \
+  [targetMenu addItem:item]; item; })
+
+
+- (void) addHelperItemsToMenu:(NSMenu*)menu asSubMenu:(BOOL)submenu {
+  
+  NSMenu *targetMenu;
+  
+  if (submenu) {
+    
+    NSMenu *moreMenu = [NSMenu.alloc initWithTitle:@"Preferences"];
+
+    NSMenuItem *moreItem = [NSMenuItem.alloc initWithTitle:@"Preferences" action:nil keyEquivalent:@""];
+    moreItem.submenu = moreMenu;
+    [menu addItem:moreItem];
+    targetMenu = moreMenu;
+
+  } else targetMenu = menu;
+  
+  // add reset, aka refreshMenuItem
+  ADD_MENU(@"Refresh ", reset, @"r", self);
+
+  [targetMenu addItem:NSMenuItem.separatorItem];
+  
+  // add edit action, aka prefsMenuItem
+  ADD_MENU(@"Change Plugin Folder…", changePluginDirectory,@"",self);
+  
+  // add edit action, aka openPluginFolderMenuItem
+  ADD_MENU(@"Open Plugin Folder…",openPluginFolder, nil, self);
+
+  // add browser item, aka openPluginBrowserMenuItem
+  ADD_MENU(@"Find More Plugins…", openPluginsBrowser, nil, self);
+
+  [targetMenu addItem:NSMenuItem.separatorItem];
+  
+  // open at login, aka openAtLoginMenuItem
+  LaunchAtLoginController *lc = LaunchAtLoginController.new;
+
+  [ADD_MENU(@"Open at Login", toggleOpenAtLogin:, nil, self) setState:lc.launchAtLogin];
+  
+  [targetMenu addItem:NSMenuItem.separatorItem];
+  
+  NSString *versionString = [NSBundle.mainBundle.infoDictionary objectForKey:@"CFBundleVersion"];
+  
+  NSMenuItem *versionMenuitem = [[NSMenuItem alloc] initWithTitle:[NSString stringWithFormat:@"v%@", versionString] action:nil keyEquivalent:@""];
+  [targetMenu addItem:versionMenuitem];
+
+  // add troubleshooting item
+  ADD_MENU(@"User Guide…", openTroubleshootingPage,@"g",self);
+  
+  // add troubleshooting item
+  ADD_MENU(@"Report an Issue…",openReportIssuesPage,@"i",self);
+  
+  // quit menu
+  ADD_MENU(@"Quit",quit, @"q",self);
+}
+
+- (void) quit {
+  [NSApp terminate:[NSApplication sharedApplication]];
+}
+#define WSPACE NSWorkspace.sharedWorkspace
+
+- (void) openReportIssuesPage {
+  [WSPACE openURL:[NSURL URLWithString:@"https://github.com/stretchr/bitbar/issues"]];
+}
+
+- (void) openPluginsBrowser {
+    [WSPACE openURL:[NSURL URLWithString:@"https://github.com/stretchr/bitbar/tree/master/Plugins"]];
+}
+
+- (void) openTroubleshootingPage {
+  [WSPACE openURL:[NSURL URLWithString:@"https://github.com/stretchr/bitbar/wiki/User-Guide"]];
+}
+
+- (void) openPluginFolder {
+  [WSPACE openURL:[NSURL fileURLWithPath:self.path]];
+}
+
+- (void) toggleOpenAtLogin:(id)sender {
+  
+  LaunchAtLoginController *lc = LaunchAtLoginController.new;
+  [lc setLaunchAtLogin:!lc.launchAtLogin];
+
+  [(NSMenuItem*)sender setState:lc.launchAtLogin];
+  
+}
+
+- (NSArray*) pluginFilesWithAsking:(BOOL)shouldAsk {
+  
+  BOOL dirIsOK = !!self.path;
+  
+  if (dirIsOK) {
+    
+    BOOL isDir;
+    if (![NSFileManager.defaultManager fileExistsAtPath:self.path isDirectory:&isDir])
+      dirIsOK = NO;
+    
+    if (!isDir) dirIsOK = NO;
+  }
+  
+  if (dirIsOK) {
+    
+    // get the listing
+    NSError *error;
+    NSArray *dirFiles = [NSFileManager.defaultManager contentsOfDirectoryAtPath:self.path error:&error];
+    
+    // handle error if there is one
+    if (error != nil) {
+      dirIsOK = NO;
+    } else {
+      // filter dot files
+      dirFiles = [dirFiles filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"NOT self BEGINSWITH '.'"]];
+      // filter markdown files
+      dirFiles = [dirFiles filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"NOT self ENDSWITH '.md'"]];
+      // filter subdirectories
+      dirFiles = [dirFiles filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id name, NSDictionary *bindings) {
+        BOOL isDir;
+        NSString * path = [self.path stringByAppendingPathComponent:name];
+        return [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir] && !isDir;
+      }]];
+      return dirFiles;
+    }
+  }
+  
+  return (!dirIsOK && shouldAsk) && self.beginSelectingPluginsDir ? [self pluginFilesWithAsking:NO] : nil;
+}
+
+- (BOOL) beginSelectingPluginsDir {
+  
+  NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+  [openDlg setCanChooseDirectories:YES];
+  [openDlg setCanChooseFiles:NO];
+  [openDlg setCanCreateDirectories:YES];
+  [openDlg setPrompt:@"Use as Plugins Directory"];
+  [openDlg setTitle:@"Select BitBar Plugins Directory"];
+  
+  if (openDlg.runModal == NSOKButton) {
+    
+    self.path = [openDlg.directoryURL path];
+    [DEFS setPluginsDirectory:self.path];
+    return YES;
+    
+  } else self.path = DEFS.pluginsDirectory;
+  
+  return NO;
+  
+}
+
+- (void) reset {
+  
+  // remove all status items
+  Plugin *plugin;
+  for (plugin in _plugins) [self.statusBar removeStatusItem:plugin.statusItem];
+  
+  _plugins = nil;
+  [self.statusBar removeStatusItem:self.defaultStatusItem];
+  [self setupAllPlugins];
+  
+}
+
+- (void) clearPathAndReset {
+  self.path = nil;
+  [self reset];
+}
+
+- (void)changePluginDirectory {
+  if ([self beginSelectingPluginsDir] == YES) {
+    [self reset];
+  }
+}
+
+- (NSArray *)plugins {
+  
+  return _plugins = _plugins ?: ({
+    
+    NSArray *pluginFiles = [self pluginFilesWithAsking:YES];
+    NSMutableArray *plugins = [NSMutableArray.alloc initWithCapacity:[pluginFiles count]];
+    NSString *file;
+    for (file in pluginFiles) {
+     
+      // setup this plugin
+      Plugin *plugin;
+      if ([@[@"html",@"htm"] containsObject:file.pathExtension.lowercaseString]) {
+        plugin = [HTMLPlugin.alloc initWithManager:self];
+      } else {
+        plugin = [ExecutablePlugin.alloc initWithManager:self];
+      }
+      
+      [plugin setPath:[self.path stringByAppendingPathComponent:file]];
+      [plugin setName:file];
+      [plugin.statusItem setTitle:@"…"];
+      
+      [plugins addObject:plugin];
+      
+    }
+    
+    plugins.copy;
+
+  });
+}
+
+- (NSDictionary *)environment {
+  
+  return _environment = _environment ?: ({
+
+    NSMutableDictionary *env = NSProcessInfo.processInfo.environment.mutableCopy;
+    env[@"BitBar"] = @YES;
+    env;
+  });
+  
+}
+
+- (void) pluginDidUdpdateItself:(Plugin*)plugin {
+  [self checkForNoPlugins];
+}
+
+- (void)checkForNoPlugins {
+  
+  Plugin *plugin;
+  NSInteger visiblePlugins = 0;
+  for (plugin in self.plugins) {
+    if (plugin.pluginIsVisible)
+      visiblePlugins++;
+  }
+  
+  visiblePlugins != 0 ? [self.statusBar removeStatusItem:self.defaultStatusItem]
+                      : [self showSystemStatusItemWithMessage:@"No plugins found"];
+}
+
+- (NSStatusBar *)statusBar { return _statusBar = _statusBar ?: NSStatusBar.systemStatusBar; }
+
+
+- (void) setupAllPlugins {
+
+  NSArray *plugins = self.plugins;
+  
+  if (!plugins.count)  [self checkForNoPlugins];
+
+  else {
+    
+    for (Plugin *plugin in plugins) [plugin refresh];
+
+    [_timerForLastUpdated invalidate];
+    self.timerForLastUpdated = [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(updatePluginLastUpdatedValues) userInfo:nil repeats:YES];
+    
+  }
+}
+
+- (void)updatePluginLastUpdatedValues {
+
+  for (Plugin *plugin in self.plugins)
+    plugin.lastUpdated  ? [plugin.lastUpdatedMenuItem setTitle:[NSString stringWithFormat:@"Updated %@", plugin.lastUpdatedString]]
+                        : [plugin.lastUpdatedMenuItem setTitle:@"Refreshing…"];
+}
+
+#pragma mark - NSMenuDelegate
+
+- (void)menuWillOpen:(NSMenu *)menu {
+  [self.defaultStatusItem setHighlightMode:YES];
+}
+
+- (void)menuDidClose:(NSMenu *)menu {
+  [self.defaultStatusItem setHighlightMode:NO];
+}
+
+@end
